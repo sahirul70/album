bbdaf7b534ffd5ab2c4632465e755746fc5d627d
sahirul70 <sahirulislam.70@gamil.com>
2021-11-27 19:00:51 +0600

    this i commsn

--- test_project/settings.py
+++ test_project/settings.py
@@ -117,11 +117,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'sopnil',
